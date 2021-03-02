5f59c6b54d6574ba8f1b079427ce74e0ba318937
lindsaykh <lindsay.hussey@gmail.com>
2021-03-02 18:45:50 -0500

    updated readme and files

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -6,8 +6,9 @@ import scrape_mars
 # Create an instance of Flask
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
+# Use PyMongo to establish Mongo connection to database called mars_db
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
@@ -15,7 +16,7 @@ def home():
 
     # Find one record of data from the mongo database
     mars_dict = mongo.db.mars_dict.find_one()
-    # Return template by looking for files in templates folder and renaming mars dict
+    # Return template by looking for files in templates folder and refer to mars dict as mars for simplicity
     return render_template("index.html", mars=mars_dict)
 
 # Route that will trigger the scrape function
@@ -26,6 +27,8 @@ def scrape():
     mars_data = scrape_mars.scrape()
     # Update the Mongo database using update and upsert=True
     mars_dict.update({}, mars_data, upsert=True)
+
+    print("Data Uploaded!")
     return redirect("/")
 
 if __name__ == "__main__":
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,8 +1,8 @@
-# with a function called scrape that will execute all of your 
-#scraping code and 
-# return one Python dictionary containing all of the scraped data.
+# The code below has a function called scrape that will execute all the scraping code from the 
+# corresponding mission_to_mars jupyter notebook scraping code. It then returns one Python dictionary 
+# containing all of the scraped data called mars_dict.
 
-# Import dependencies 
+# Import all necessary dependencies 
 import pandas as pd
 from bs4 import BeautifulSoup as bs
 import requests
@@ -11,51 +11,73 @@ from splinter import Browser
 from flask import Flask, render_template, redirect
 from webdriver_manager.chrome import ChromeDriverManager
 
-# initialize the browser for the scrape
+# initialize the browser for the scrape (installs as needed)
 def init_browser():
-    executable_path = {'executeable_path': 'chromedriver'}
-    return Browser('chrome', **executable_path, headless=False)
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    # executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    return Browser("chrome", **executable_path, headless=False)
 
+# define scrape function based on jupyter notebook components
 def scrape():
     browser = init_browser()
+    
     # initialize dictionary for scraped data
     mars_dict = {}
 
-    # scrape latest news from nasa mars
+    ### scrape latest news from nasa mars ###
+    # url info
     nasa_mars_url = "https://mars.nasa.gov/news"
     browser.visit(nasa_mars_url)
     html = browser.html
     soup = bs(html,'html.parser')
 
-    # grab latest news title
+    # grab latest news title with beautiful soup find_all text within appropriate div class
     news_title = soup.find_all('div', class_='content_title')[1].text
 
-    # grab corresponding paragraph
-    news_p = soup.find_all('div', class_='article_teaser_body')[0].text
+    # grab corresponding paragraph with beautiful soup find_all text within appropriate div class
+    news_p = soup.find_all('div', class_='article_teaser_body')[1].text
     
-    # jpl featured image scrape
-    jpl_url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html"
+    ### jpl featured image scrape ###
+    # url info
+    jpl_url = "https://www.jpl.nasa.gov/images"
     browser.visit(jpl_url)
     html = browser.html
     soup = bs(html,'html.parser')
-    featured_image = soup.find('div',class_='floating_text_area').a['href']
+
+    # grab featured image url from <a tag in appropriate div class
+    featured_image = soup.find('div',class_='SearchResultCard').a['href']
+
+    # append grabbed featured image url to main url to get image url
     featured_image_url = jpl_url + featured_image
 
-    # mars facts scrape
+    ### mars facts scrape ###
+    # url info
     mars_facts_url = "https://space-facts.com/mars/"
     browser.visit(mars_facts_url)
+
+    # read table and create data frame with new column ids and reset index
     mars_table = pd.read_html(mars_facts_url)
-    mars_facts = pd.DataFrame(mars_table)
+    mars_facts = mars_table[0]
+    mars_facts = mars_facts.rename(columns = {0:"Description", 1: "Value"})
+    mars_facts.set_index("Description",inplace=True)
+
+    # Use Pandas to convert the data to a HTML table string.
     mars_facts_html = mars_facts.to_html()
     mars_facts_html.replace('\n','')
 
-    # mars hemisphere image scrape
+    ### mars hemisphere image scrape ###
+    # url info
     mars_hemisphere_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(mars_hemisphere_url)
     html = browser.html
     soup = bs(html,'html.parser')
+
+    # use beautiful soup to grab hemisphere images from the main page (all are in collapsible results)
     mars_hemispheres = soup.find('div',class_='collapsible results')
-    mars_item = mars_hemispheres.find_all('div',class_='item')
+    mars_item = mars_hemispheres.find('div',class_='item')
+    
+    # initialize array
     hemisphere_image_urls = []
 
     # loop through each item to get title and image url
@@ -75,7 +97,7 @@ def scrape():
             image_url = soup.find('li').a['href']
         
             if (title and image_url):
-                # print results
+                # print results in terminal when running (similar to jupyter notebook)
                 print('-------------------------------------')
                 print(title)
                 print(image_url)
@@ -87,14 +109,15 @@ def scrape():
             }
             hemisphere_image_urls.append(hemisphere_dict)
         except Exception as e:
+            # print errors in terminal if they show up
             print(e)
         
         # create dictionary for scraped data above
         mars_dict = {
             "news_title": news_title,
             "news_p": news_p,
-            "featured_image_url":featured_image_url,
-            "mars_facts_html":mars_facts_html,
+            "featured_image":featured_image_url,
+            "mars_facts":mars_facts_html,
             "hemisphere_images": hemisphere_image_urls
         }
 
